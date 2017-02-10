ab60d73a00a530a9233915398bcaf50b3383febb
dimashenkov <dimashenkov@abv.bg>
2017-02-10 14:45:03 +0000

    adding enviroment

--- cookbooks/apache/recipes/websites.rb
+++ cookbooks/apache/recipes/websites.rb
@@ -1,6 +1,11 @@
 file 'default www' do
 	path '/var/www/html/index.html'
-	content 'ZDRASTI!!'
+	content 'ZDRASTI!! v.2.0.0'
 
 end
 
+webnodes = search('node', 'role:web')
+
+webnodes.each do |node|
+	puts node
+end
