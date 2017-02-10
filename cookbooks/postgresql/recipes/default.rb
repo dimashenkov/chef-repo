5951833af8b1a16e5da4f3c01786c17898f8dff5
dimashenkov <dimashenkov@abv.bg>
2017-02-10 11:05:19 +0000

    adding postgress

--- cookbooks/postgresql/recipes/default.rb
+++ cookbooks/postgresql/recipes/default.rb
@@ -7,7 +7,7 @@
 
 
 package 'postgresql-server' do
-	notifies :run, 'execute[postgresql-init]'
+	notifies :run, 'execute[postgresql-init]' , :immediately
 end
 
 execute 'postgresql-init' do
@@ -15,7 +15,7 @@ execute 'postgresql-init' do
 	action :nothing
 end
 
-service 'postgressql' do
+service 'postgresql' do
 	action [:enable, :start]
 end
 
